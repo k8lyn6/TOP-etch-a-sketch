82e2f39dec96544697c391f6e0c6985acef44b85
k8lyn6 <kaitlyn.sisk@gmail.com>
2020-08-18 14:08:04 -0500

    working on resetting

--- etchasketch.js
+++ etchasketch.js
@@ -1,15 +1,42 @@
+//Initialize constant variables
 const newColor = "#800080";
-const allBoxes = document.getElementsByClassName("grid-box");
+const gridContainer = document.getElementById("grid-container");
+let allBoxes = document.getElementsByClassName("grid-box");
+const gridButton = document.getElementById("grid-button");
+let gridRow = 16;
+let gridSize = gridRow * gridRow;
 
-function createGrid(){
-    for (i=0; i < 256; i++)
-    {
-        var gridBox = document.createElement('div');
-        gridBox.setAttribute("class", "grid-box");
-        document.getElementById("grid-container").appendChild(gridBox);
+createGrid(gridSize);
+
+gridButton.addEventListener("click", function(){
+    gridRow = prompt("Please enter the size of the grid", "16");
+    gridRow = parseInt(gridRow);
+    gridSize = gridRow * gridRow;
+    console.log(gridSize);
+    clearGrid();
+    createGrid(gridSize);
+    setNewColor();
+})
+
+function clearGrid(){
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
     }
 }
 
+function createGrid(inputSize){   
+    for (let i=0; i < inputSize; i++)
+    {
+        var gridBox = document.createElement('div');
+        gridBox.setAttribute("class", "grid-box");
+        gridContainer.appendChild(gridBox);
+    }  
+
+    allBoxes = document.getElementsByClassName("grid-box");
+    console.log(allBoxes);
+    //allBoxes.style.width = (parseInt(gridContainer.style.width) / Math.sqrt(inputSize)) + "px";
+    //allBoxes.style.height = allBoxes.style.width;
+}
 
 function setNewColor(){
     for (let i=0; i < allBoxes.length; i++){
@@ -19,5 +46,4 @@ function setNewColor(){
     }
 };
 
-createGrid();
 setNewColor();
